3b9aea7d1957ce3a7fd34f597d50f22890dc2f82
Ritvick <ritvick@users.noreply.github.com>
2023-03-16 12:37:58 -0400

    15202 - Auth-web allows incorrect province in contacts which causes e… (#320)
    * 15202 - Auth-web allows incorrect province in contacts which causes error in create_cfs_account_job
    
    * removed empty line
    
    * updated package number

--- vue/sbc-common-components/src/components/BaseAddress.vue
+++ vue/sbc-common-components/src/components/BaseAddress.vue
@@ -123,6 +123,7 @@
                     v-model="addressLocal.addressCountry"
                     :items="getCountries()"
                     :rules="[...rules.addressCountry, ...spaceRules]"
+                    v-on:change="resetRegion()"
           />
           <!-- special field to select AddressComplete country, separate from our model field -->
           <input type="hidden" :id="addressCountryId" :value="addressCountry" />
@@ -197,6 +198,10 @@ export default class BaseAddress extends Mixins(ValidationMixin, CountriesProvin
 
   @Prop({ default: false })
   readonly noPoBox: boolean
+
+  resetRegion() {
+    this.addressLocal['addressRegion'] = ''
+  }
 
   /** A local (working) copy of the address, to contain the fields edited by the component (ie, the model). */
   private addressLocal: object = {}
