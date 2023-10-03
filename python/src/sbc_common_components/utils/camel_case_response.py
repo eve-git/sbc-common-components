1aa7dc8ed3897ad4bf679c789ac6c66c88550bfe
Travis Semple <travis8814@gmail.com>
2023-10-03 11:45:07 -0700

    Fix serialize array (#335)
    * Fix serialize array
    
    * Fix type hint
    
    * use list comprehension

--- python/src/sbc_common_components/utils/camel_case_response.py
+++ python/src/sbc_common_components/utils/camel_case_response.py
@@ -39,9 +39,12 @@ def camelcase(string):
     ])
 
 
-def camelcase_dict(data: Dict[str, any], camel_dict: Dict[str, any]):
+def camelcase_dict(data, camel_dict: Dict[str, any]):
     """Iterate through the dict and convert to camel case."""
     if data:
+        # Handle the scenario where we aren't a dict
+        if isinstance(data, list):
+            return [camelcase_dict(item, {}) for item in data]
         for key, value in data.items():
             key = camelcase(key)
             if isinstance(value, dict):
