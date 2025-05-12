d280cd1f9cba1ff1a24f5fb823ba089a8ad66e2a
Travis Semple <travis8814@gmail.com>
2025-05-12 13:59:10 -0700

    Add interceptor for gateway (optional) (#393)

--- vue/sbc-common-components/src/util/interceptors.ts
+++ vue/sbc-common-components/src/util/interceptors.ts
@@ -4,6 +4,10 @@ import { SessionStorageKeys } from './constants'
 
 export function addAxiosInterceptors (axiosInstance: AxiosInstance): AxiosInstance {
   axiosInstance.interceptors.request.use(config => {
+    const authGatewayApiKey = import.meta.env.VUE_APP_AUTH_API_KEY
+    if (authGatewayApiKey && config.url.includes(ConfigHelper.getAuthAPIUrl())) {
+      config.headers['x-apikey'] = authGatewayApiKey
+    }
     const token = ConfigHelper.getFromSession(SessionStorageKeys.KeyCloakToken)
     if (token) {
       config.headers.Authorization = `Bearer ${token}`
